a7770cb2f8e1250331212ecc80e5e64054a77112
Sander <sanderpajusalu@gmail.com>
2022-09-24 11:52:04 +0300

    Update launch_cromwell.sh

--- launch_cromwell.sh
+++ launch_cromwell.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -c 8
+#SBATCH -c 2
 #SBATCH -J launch_cromwell
-#SBATCH --mem=64000
+#SBATCH --mem=4000
+#SBATCH -t 96:00:00
 
-java -Dconfig.file=/gpfs/ycga/project/ysm/lek/shared/tools/cromwell_wdl/slurm.conf -jar \
-/gpfs/ycga/project/ysm/lek/shared/tools/jars/cromwell-36.jar run \
-/gpfs/ycga/project/ysm/lek/shared/tools/RNAseq/RNAseq.wdl \
+
+module load perl/5.30.1 python/3.8.6
+
+#module load python/3.6.3/virtenv
+#module load star-2.7
+#module load perl-5.22.0
+
+java -Dconfig.file=/gpfs/space/home/a71644/tools/cromwell_wdl/slurm.conf -jar \
+/gpfs/space/home/a71644/tools/jars/cromwell-36.jar run \
+/gpfs/space/home/a71644/tools/RNAseq/RNAseq.wdl \
 -i RNAseq.inputs.json \
 -o cromwell.options
